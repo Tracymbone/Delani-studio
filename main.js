bc27fb71586f283c686d510171f74a028391d5d3
moringa <tressy.mbone@student.moringaschool.com>
2021-11-30 23:32:29 +0300

    modifyied html css js

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
-    $("#designimage").click(function() {
+    $("#design").click(function() {
         $("#designtext").toggle();
-        $("#designimage").toggle();
+
     })
 })
+$(document).ready(function() {
+    $("#development").click(function() {
+        $("#developmenttext").toggle();
+
+    })
+})
+$(document).ready(function() {
+    $("#projectmanagement").click(function() {
+        $("#projectmanagementtext").toggle();
+
+    })
+})
+
+// $(document).ready(function(){
+//     $(".blankdivs").hover(function(){
+//       $(this).css("background-color", "rgba(0,0,0,0.5)");
+//       $(".assets").css("color","white");
+//       }, function(){
+//       $(this).css("background-color", "inherit");
+//     });
+//   });
+ 
+  $(document).ready(function(){
+    $(".blankdivs").hover(function(){
+      $(this).css("background-color", "rgba(0,0,0,0.5)");
+      $(".assets").css("color","white");
+$(".asset s").css("border", "2px solid white");
+      }, function(){
+      $(this).css("background-color", "transparent");
+$(".assets").css("color", "inherit");
+$(".assets").css("border", "2px solid transparent");
+    });
+  });
+
