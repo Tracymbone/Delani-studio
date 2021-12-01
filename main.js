fb0ed77240d18e2e4ca3c58fa3d046ce294cdacb
moringa <tressy.mbone@student.moringaschool.com>
2021-12-01 20:48:48 +0300

    modified html

--- main.js
+++ main.js
@@ -16,15 +16,97 @@ $(document).ready(function() {
 
     })
 })
+
+// $(document).ready(function(){
+//     $(".blankdivs").hover(function(){
+//       $(this).css("background-color", "rgba(0,0,0,0.5)");
+//       $(".assets").css("color","white");
+//       }, function(){
+//       $(this).css("background-color", "inherit");
+//     });
+//   });
+ 
+//   $(document).ready(function(){
+//     $(".blankdivs").hover(function(){
+//       $(this).css("background-color", "rgba(0,0,0,0.5)");
+//       $("#assets").css("color","white");
+//       $("#assets").css("border", "2px solid white");
+//       }, function(){
+//       $(this).css("background-color", "transparent");
+//       $("#assets").css("color", "transparent");
+//       $("#assets").css("border", "2px solid transparent");
+//     });
+//   });
+
   $(document).ready(function(){
-    $(".blankdivs").hover(function(){
+    $(".blankdivs1").hover(function(){
       $(this).css("background-color", "rgba(0,0,0,0.5)");
-      $(".assets").css("color","white");
-$(".asset s").css("border", "2px solid white");
+      $("#assets1").show();
       }, function(){
       $(this).css("background-color", "transparent");
-$(".assets").css("color", "inherit");
-$(".assets").css("border", "2px solid transparent");
+      $("#assets1").hide();
     });
   });
-
+  $(document).ready(function(){
+    $(".blankdivs2").hover(function(){
+      $(this).css("background-color", "rgba(0,0,0,0.5)");
+      $("#assets2").show();
+      }, function(){
+      $(this).css("background-color", "transparent");
+      $("#assets2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".blankdivs3").hover(function(){
+      $(this).css("background-color", "rgba(0,0,0,0.5)");
+      $("#assets3").show();
+      }, function(){
+      $(this).css("background-color", "transparent");
+      $("#assets3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".blankdivs4").hover(function(){
+      $(this).css("background-color", "rgba(0,0,0,0.5)");
+      $("#assets4").show();
+      }, function(){
+      $(this).css("background-color", "transparent");
+      $("#assets4").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".blankdivs5").hover(function(){
+      $(this).css("background-color", "rgba(0,0,0,0.5)");
+      $("#assets5").show();
+      }, function(){
+      $(this).css("background-color", "transparent");
+      $("#assets5").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".blankdivs6").hover(function(){
+      $(this).css("background-color", "rgba(0,0,0,0.5)");
+      $("#assets6").show();
+      }, function(){
+      $(this).css("background-color", "transparent");
+      $("#assets6").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".blankdivs7").hover(function(){
+      $(".blankdivs7").css("background-color", "rgba(0,0,0,0.5)");
+      $("#assets7").show();
+      }, function(){
+      $(".blankdivs7").css("background-color", "transparent");
+      $("#assets7").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".blankdivs8").hover(function(){
+      $(this).css("background-color", "rgba(0,0,0,0.5)");
+      $("#assets8").show();
+      }, function(){
+      $(this).css("background-color", "transparent");
+      $("#assets8").hide();
+    });
+  });
